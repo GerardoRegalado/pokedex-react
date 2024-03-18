82ab1ed3075bc3c673085e141d2b97681b24f043
Gerardo Regalado <gerardo.regalado24@gmail.com>
2024-03-18 11:41:48 -0600

    Feat(): Setting up app component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import Pokedex from './components/shared/pokedex';
+
 function App() {
+  const URL = 'https://pokeapi.co/api/v2/'
+  const [pokemon, setPokemon] = useState([])
+
+  useEffect(() => {
+    const APIconnection = async () => {
+      try {
+        const response = await axios.get(URL + 'pokemon?limit=11&offset=0')
+        const data = response.data.results
+        const promises = data.map(pokemon => axios.get(URL + `pokemon/${pokemon.name}` ))
+        const results = await Promise.all(promises)
+        const pokemones = results.map(result => result.data)
+        setPokemon(pokemones)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    APIconnection();
+  },[])
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Pokedex 
+      pokemon = {pokemon}
+      />
     </div>
   );
 }
 
 export default App;
+
